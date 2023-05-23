e287276f6a06ca2821e9548ebc220a41edd96a82
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2023-05-23 18:31:05 +0200

    add initial docs

---
+++ assets/js/1124c7c5.051d3eaf.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpterodactyl=self.webpackChunkpterodactyl||[]).push([[95],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ assets/js/343.74f94e6f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpterodactyl=self.webpackChunkpterodactyl||[]).push([[343],{3343:(e,t,c)=>{c.r(t)}}]);
