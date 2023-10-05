70302f15ae03797ffcf83917f0cfe9f1fcbd4fcd
Quinten <josdekurk@gmail.com>
2023-10-05 10:06:17 +0200

    trailingSlash true

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,8 @@ const config = {
   title: 'Pterodactyl',
   tagline: 'Pterodactyl is an open-source game server management panel built with PHP, React, and Go. Designed with security in mind, Pterodactyl runs all game servers in isolated Docker containers while exposing a beautiful and intuitive UI to end users.',
   favicon: 'img/favicon.ico',
-
+  trailingSlash: true,
+  
   // Set the production url of your site here
   url: 'https://quintenqvd0.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
