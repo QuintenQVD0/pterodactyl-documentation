c02b44031823d5fd76b27a3d6cee7791e8d6f307
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2023-11-08 10:43:12 +0100

    new social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/bannner_logo.png',
+      image: 'img/social-card.png',
       navbar: {
         title: 'Pterodactyl',
         logo: {
