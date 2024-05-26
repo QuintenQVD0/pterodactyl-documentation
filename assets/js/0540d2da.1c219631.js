8f9712366055396826e534b7000ae6c3c4c33054
actions-user <action@github.com>
2024-05-26 11:12:09 +0000

    Deploy website - based on b106440bafedd65dbe68ea3840eedbc3b649b7a0

--- assets/js/0540d2da.1c219631.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkpterodactyl=self.webpackChunkpterodactyl||[]).push([[9242],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ assets/js/a7456010.8bb176f2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpterodactyl=self.webpackChunkpterodactyl||[]).push([[5980],{9365:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
